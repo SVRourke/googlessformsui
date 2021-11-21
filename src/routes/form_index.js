051b7a3d05a797f8d4163f0d2ba8c234d25275f7
Samuel Rourke <samuelvanrourke@gmail.com>
2021-11-21 18:49:58 -0500

    experimenting with requesting data

--- src/routes/form_index.js
+++ src/routes/form_index.js
@@ -1,9 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+import { Link } from "react-router-dom";
+import { formIndex } from "../lib/api";
+
 function FormIndex() {
+  let [forms, setForms] = useState([]);
+
+  // formIndex(1)
+  console.log("STATE", forms);
+  let formLinks;
+
+  useEffect(() => {
+    setForms(formIndex(1));
+    formLinks = forms.map((form) => {
+      console.log(form);
+      return <Link to={form.id}>{form.title}!</Link>;
+    });
+  }, []);
+
+  console.log(forms);
+
   return (
     <div>
       <header>
         <h1>Form Name</h1>
-        <h3>Submissions</h3>
+        <Link to="2">Form 2</Link>
+        <Link to="3">Form 3</Link>
+        <Link to="4">Form 4</Link>
+        {formLinks}
       </header>
     </div>
   );
