9f25a80b37d237012a503582d844fcd0084d1a93
SVRourke <samuelvanrourke@gmail.com>
2021-11-22 12:36:05 -0500

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 
+import { store } from "./store";
 import { Provider } from "react-redux";
-import store from "./store";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
 ReactDOM.render(
