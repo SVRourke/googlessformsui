b87268c623d342da69d9c3dce232fad67fabb7d6
SVRourke <samuelvanrourke@gmail.com>
2021-11-22 16:22:58 -0500

    function for creating a user

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,19 @@
 const BASEURL = "http://localhost:3000";
 
-exports.formIndex = async (userId) => {
-  const endpoint = `${BASEURL}/users/${userId}/forms`;
-  const req = await fetch(endpoint, { method: "GET" });
-  return req.json()
+// exports.formIndex = async (userId) => {
+//   const endpoint = `${BASEURL}/users/${userId}/forms`;
+//   const req = await fetch(endpoint, { method: "GET" });
+//   return req.json()
+// };
+
+export const createUser = async (info) => {
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ user: info }),
+  };
+
+  const req = await fetch("http://localhost:3000/users", options);
+
+  return req.json();
 };
