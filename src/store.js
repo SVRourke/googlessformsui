11315792978a82ce72057231534dd9c8efe9685b
SVRourke <samuelvanrourke@gmail.com>
2021-11-22 12:36:17 -0500

    reworking store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,9 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit";
 
-export default configureStore({
-    reducer: {},
-})
+import userReducer from "./redux/userSlice";
+
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
