2f13c77d1c2ecefd292be667a10e8751e4f43314
SVRourke <samuelvanrourke@gmail.com>
2021-11-23 16:39:03 -0500

    created a page index

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./home";
+import FormIndex from "./form_index";
+import NewForm from "./new_form";
+import FormPage from "./form_page";
+
+export { Home, FormIndex, NewForm, FormPage };
+
+// Login / Sign Up
+// New Form
+
