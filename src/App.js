776a579745c0cc684dd2fa5880ff990252646d7a
SVRourke <samuelvanrourke@gmail.com>
2021-11-23 16:37:15 -0500

    restructured page imports, added menu component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./routes/home";
-import FormIndex from "./routes/form_index";
-import NewForm from "./routes/new_form";
-import FormPage from "./routes/form_page";
+import { Home, FormIndex, NewForm, FormPage } from "./pages";
+import Menu from "./components/menu";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Menu />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/forms" element={<FormIndex />} />
         <Route path="/forms/:id" element={<FormPage />} />
         <Route path="/forms/new" element={<NewForm />} />
