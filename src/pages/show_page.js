440ca0d6ca0023de7008419e0bd3d1a75b73a439
SVRourke <samuelvanrourke@gmail.com>
2021-11-24 10:48:47 -0500

    updated to use sample data

--- src/pages/show_page.js
+++ src/pages/show_page.js
@@ -1,8 +1,14 @@
+import { useParams } from "react-router";
+import forms from "../sampledata/forms";
+
 function ShowForm() {
+  const {id} = useParams()
+  const form = forms.select(f => i[:id] == id)
   return (
     <div>
       <header>
         <h1>Show Form Page</h1>
+        <p>{form["title"]}</p>
       </header>
     </div>
   );
