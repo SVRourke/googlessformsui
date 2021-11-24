d0cdfa36cd7424d5b34df0f39afab25bdf75948f
SVRourke <samuelvanrourke@gmail.com>
2021-11-24 10:49:30 -0500

    updated to use sample data

--- src/pages/form_index.js
+++ src/pages/form_index.js
@@ -1,33 +1,20 @@
 import React, { useState, useEffect } from "react";
 
-import { Link } from "react-router-dom";
 import { formIndex } from "../lib/api";
 
+import { FormCard } from "../components";
+import forms from "../sampledata/forms";
+
+import CreateForm from "../forms/CreateForm";
+
 function FormIndex(props) {
-
-
-  // let [forms, setForms] = useState([]);
-  // formIndex(1)
-  // console.log("STATE", forms);
-  // let formLinks;
-
-  // useEffect(() => {
-  //   setForms(formIndex(1));
-  //   formLinks = forms.map((form) => {
-  //     console.log(form);
-  //     return <Link to={form.id}>{form.title}!</Link>;
-  //   });
-  // }, []);
-
+  // order by date later
+  const cards = forms.map((form) => <FormCard form={form} />);
   return (
     <div>
-      <header>
-        <h1>Form Name</h1>
-        <Link to="2">Form 2</Link>
-        <Link to="3">Form 3</Link>
-        <Link to="4">Form 4</Link>
-        {/* <p>{state}</p> */}
-      </header>
+      <h1>Forms</h1>
+      <CreateForm />
+      <ul>{cards}</ul>
     </div>
   );
 }
