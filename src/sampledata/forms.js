93f70fdd899d4e5755575cb9507adee87a13d908
SVRourke <samuelvanrourke@gmail.com>
2021-11-24 10:48:13 -0500

    added sample data

---
+++ src/sampledata/forms.js
@@ -0,0 +1,9 @@
+const forms = [
+  { id: 1, title: "Survey 1" },
+  { id: 2, title: "Survey 2" },
+  { id: 3, title: "Survey 3" },
+  { id: 4, title: "Survey 4" },
+  { id: 5, title: "Survey 5" },
+];
+
+export default forms;
